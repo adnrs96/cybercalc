1f411ab8d74711bb7136a01f7a3a7066340a819f
Aditya Bansal <adi.bansal241996@gmail.com>
2016-06-15 12:56:59 +0530

    Added basic parser for bracket resolution

--- modules/arithmatic.py
+++ modules/arithmatic.py
@@ -1,3 +1,25 @@
 #This module will provide one stop solution to and string expression composed of operands and operators between +,-,/,* and curved braces ()
 
+def earth(s):
+	
+	#parsing to simplify the expression by removing brackets
+	op = -1,cls=-1
+	for i in range(0,len(s)):
+		if s[i] == '(':
+			op=i
+		if s[i] == ')':
+			cls=i
+			if op==-1:
+				#Raise Exception
+			simplify=evaluate(s[op+1:cls])
+			if simplify==-1:
+				#Raise Exception
+			s=s[0:op]+simplify+s[cls+1:]
+			i=0,op=-1,cls=-1
+	
+	s=evaluate(s)
+	if s==-1:
+		#Raise Exception
+	return s
+	
 
