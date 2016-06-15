6e0c26ee0cb0a8117feaa9f59cd173708052e4e5
Aditya Bansal <adi.bansal241996@gmail.com>
2016-06-15 16:39:37 +0530

    Added parenthesis matching

--- modules/arithmatic.py
+++ modules/arithmatic.py
@@ -10,17 +10,17 @@ def earth(s):
 			op=i
 		if s[i] == ')':
 			cls=i
-			#if op==-1:
-				#Raise Exception
+			if op==-1:
+				s=-1
+				break
 			simplify=evaluate.solve(s[op+1:cls])
-			#if simplify==-1:
-				#Raise Exception
 			s=s[0:op]+simplify+s[cls+1:]
 			i=0;op=-1;cls=-1
-	
-	s=evaluate.solve(s)
-	#if s==-1:
-		#Raise Exception
+	if op!=-1 or cls!=-1:
+		s=-1
+		return s
+	if s!=-1:
+		s=evaluate.solve(s)
 	return s
 	
 
