678f106d6bc248f02bb4469789ab3a31e9ef4b63
Aditya Bansal <adi.bansal241996@gmail.com>
2016-06-16 04:08:28 +0530

    Added Regex for Standard Expression Validation

---
+++ modules/expressionValidation.py
@@ -0,0 +1,69 @@
+#This modules provides solution to the problem of validating the string arithmatic expression for evaluation
+#Module uses RegEx to try on some tests on the input string to declare it to be valid or other wise
+import re
+def validate(s):
+	match=re.search(r'[^0-9+-/\*()]',s)
+	if match:
+		return -1
+	match=re.search(r'\)\(',s)
+	if match:
+		return -1
+	match=re.search(r'\(/',s)
+	if match:
+		return -1
+	match=re.search(r'\(*',s)
+	if match:
+		return -1
+	match=re.search(r'\.[0-9]*\.',s)
+	if match:
+		return -1
+	match=re.search(r'\+\)',s)
+	if match:
+		return -1
+	match=re.search(r'\-\)',s)
+	if match:
+		return -1
+	match=re.search(r'/\)',s)
+	if match:
+		return -1
+	match=re.search(r'\*\)',s)
+	if match:
+		return -1
+	match=re.search(r'/\*',s)
+	if match:
+		return -1
+	match=re.search(r'\*/',s)
+	if match:
+		return -1
+	match=re.search(r'\+/',s)
+	if match:
+		return -1
+	match=re.search(r'//',s)
+	if match:
+		return -1
+	match=re.search(r'\*\*',s)
+	if match:
+		return -1
+	match=re.search(r'\+\*',s)
+	if match:
+		return -1
+	match=re.search(r'\-/',s)
+	if match:
+		return -1
+	match=re.search(r'\-\*',s)
+	if match:
+		return -1
+	match=re.search(r'(/\+/)|(/\+\*)|(/\+\+)|(/\+\-)|(/\-/)|(/\-\*)|(/\-\+)|(/\-\-)',s)
+	if match:
+		return -1
+	match=re.search(r'(\*\+/)|(\*\+\*)|(\*\+\+)|(\*\+\-)|(\*\-/)|(\*\-\*)|(\*\-\+)|(\*\-\-)',s)
+	if match:
+		return -1
+	match=re.search(r'(\+\+/)|(\+\+\*)|(\+\+\+)|(\+\+\-)|(\+\-/)|(\+\-\*)|(\+\-\+)|(\+\-\-)',s)
+	if match:
+		return -1
+	match=re.search(r'(\-\+/)|(\-\+\*)|(\-\+\+)|(\-\+\-)|(\-\-/)|(\-\-\*)|(\-\-\+)|(\-\-\-)',s)
+	if match:
+		return -1
+	
+	return 1
