ed2a177b630dc72fade5a1c30dd7493db0949446
Aditya Bansal <adi.bansal241996@gmail.com>
2016-06-17 02:08:45 +0530

    Added Regex validation using module

--- modules/arithmatic.py
+++ modules/arithmatic.py
@@ -1,9 +1,13 @@
 #This module will provide one stop solution to and string expression composed of operands and operators between +,-,/,* and curved braces ()
 import evaluate
-
+import expressionValidation
 def earth(s):
 	
 	#parsing to simplify the expression by removing brackets
+	if expressionValidation.validate(s)==0:
+		s="Invalid Expression"
+		return s
+		
 	op=-1;cls=-1
 	for i in range(0,len(s)):
 		if s[i] == '(':
@@ -11,15 +15,15 @@ def earth(s):
 		if s[i] == ')':
 			cls=i
 			if op==-1:
-				s=-1
+				s="Invalid Expression"
 				break
 			simplify=evaluate.solve(s[op+1:cls])
 			s=s[0:op]+simplify+s[cls+1:]
 			i=0;op=-1;cls=-1
 	if op!=-1 or cls!=-1:
-		s=-1
+		s="Invalid Expression"
 		return s
-	if s!=-1:
+	if s!="Invalid Expression":
 		s=evaluate.solve(s)
 	return s
 	
