91ae72c03ad7d09b55aa1400e92b163d9de1fe18
Aditya Bansal <adi.bansal241996@gmail.com>
2016-06-20 19:57:42 +0530

    Regex Functinal,Corrected logic in looping,Some GUI Tweeks,Improved UI

--- cybercalc.py
+++ cybercalc.py
@@ -122,7 +122,7 @@ def validate(s):
 	match=re.search(r'\(/',s)
 	if match:
 		return 0
-	match=re.search(r'\(*',s)
+	match=re.search(r'\(\*',s)
 	if match:
 		return 0
 	match=re.search(r'\.[0-9]*\.',s)
@@ -181,12 +181,12 @@ def validate(s):
 def earth(s):
 	
 	#parsing to simplify the expression by removing brackets
-	#if validate(s)==0:
-	#	s="Invalid Expression"
-	#	return s
+	if validate(s)==0:
+		s="Invalid Expression"
+		return s
 		
-	op=-1;cls=-1
-	for i in range(0,len(s)):
+	op=-1;cls=-1;i=0
+	while i<len(s) :
 		if s[i] == '(':
 			op=i
 		if s[i] == ')':
@@ -196,7 +196,8 @@ def earth(s):
 				break
 			simplify=solve(s[op+1:cls])
 			s=s[0:op]+simplify+s[cls+1:]
-			i=0;op=-1;cls=-1
+			i=-1;op=-1;cls=-1
+		i=i+1
 	if op!=-1 or cls!=-1:
 		s="Invalid Expression"
 		return s
@@ -225,33 +226,49 @@ def action_solve(expr_inp):
 		s="Invalid Expression"
 	expr_inp.delete(0,END)
 	expr_inp.insert(0,s)
-	action_text_control=1
+	action_text_control=0
 def switch_areas():
 	f1.pack_forget()
 	f2.pack_forget()
 	f3.pack_forget()
 	areas.pack(fill=BOTH,expand=1)
 	rasq.select()
+	switch_areas_sq()
 	#area_tri.pack(fill=BOTH,expand=1)
-	area_sq.pack(fill=BOTH,expand=1)
+	#area_sq.pack(fill=BOTH,expand=1)
 	#area_rec.pack(fill=BOTH,expand=1)
 def switch_areas_tri():
 	area_sq.pack_forget()
 	area_rec.pack_forget()
+	ate1.delete(0,END)
+	ate2.delete(0,END)
+	ate3.delete(0,END)
+	ate4.delete(0,END)
 	area_tri.pack(fill=BOTH,expand=1)
 def switch_areas_sq():
 	area_tri.pack_forget()
 	area_rec.pack_forget()
+	aqe1.delete(0,END)
+	aqe2.delete(0,END)
 	area_sq.pack(fill=BOTH,expand=1)
 def switch_areas_rec():
 	area_tri.pack_forget()
 	area_sq.pack_forget()
+	are1.delete(0,END)
+	are2.delete(0,END)
+	are3.delete(0,END)
 	area_rec.pack(fill=BOTH,expand=1)
 def switch_peri_peri():
 	peri_semi.pack_forget()
+	pre1.delete(0,END)
+	pre2.delete(0,END)
+	pre3.delete(0,END)
 	peri_peri.pack(fill=BOTH,expand=1)
 def switch_peri_semi():
 	peri_peri.pack_forget()
+	spe1.delete(0,END)
+	spe2.delete(0,END)
+	spe3.delete(0,END)
 	peri_semi.pack(fill=BOTH,expand=1)
 def switch_peris():
 	f1.pack_forget()
@@ -259,58 +276,42 @@ def switch_peris():
 	f3.pack_forget()
 	peris.pack(fill=BOTH,expand=1)
 	pesq.select()
+	switch_peri_peri()
 	#area_tri.pack(fill=BOTH,expand=1)
-	peri_peri.pack(fill=BOTH,expand=1)
+	#peri_peri.pack(fill=BOTH,expand=1)
 	#area_rec.pack(fill=BOTH,expand=1)
 def switch_persqrt():
 	f1.pack_forget()
 	f2.pack_forget()
 	f3.pack_forget()
+	pqe1.delete(0,END)
+	pqe2.delete(0,END)
 	persqrt.pack(fill=BOTH,expand=1)
-def find_perimeter(n,s):
-	return n*s
 def find_semiperimeter(n,s):
 	return find_perimeter(n,s)/2
 def find_peri(pre1,pre2,pre3):
 	n=pre1.get()
 	s=pre2.get()
+	n='('+n+')'
+	s='('+s+')'
 	try:
-		n=int(n)
+		s=earth('('+n+'*'+s+')')
 	except:
-		s='Invalid No. of Sides'
-		pre3.delete(0,END)
-		pre3.insert(0,s)
-		return
-	try:
-		s=float(s)
-	except:
-		s="Invalid Length"
-		pre3.delete(0,END)
-		pre3.insert(0,s)
-		return
-	s=str(find_perimeter(n,s))
+		s="Invalid Input"
 	pre3.delete(0,END)
 	pre3.insert(0,s)
 def find_semi(pre1,pre2,pre3):
 	n=pre1.get()
 	s=pre2.get()
+	n='('+n+')'
+	s='('+s+')'
 	try:
-		n=int(n)
+		s=earth('(('+n+'*'+s+')/2)')
 	except:
-		s='Invalid No. of Sides'
-		pre3.delete(0,END)
-		pre3.insert(0,s)
-		return
-	try:
-		s=float(s)
-	except:
-		s="Invalid Length"
-		pre3.delete(0,END)
-		pre3.insert(0,s)
-		return
-	s=str(find_semiperimeter(n,s))
+		s="Invalid Input"
 	pre3.delete(0,END)
-	pre3.insert(0,s)		
+	pre3.insert(0,s)	
+	
 def find_area_triangle(a,b,c):
 	a=float(a)
 	b=float(b)
@@ -402,16 +403,24 @@ f2=Frame(root,background="#FFFFFF")
 f2.pack(fill=BOTH,expand=1,ipadx=0,ipady=0)
 f3=Frame(root,background="#434343")
 f3.pack(fill=BOTH,expand=1)
-areas=Frame(root,background="#000000")
-area_tri=Frame(root,background="#000000")
-area_sq=Frame(root,background="#000000")
-area_rec=Frame(root,background="#000000")
-peris=Frame(root,background="#000000")
-peri_peri=Frame(root,background="#000000")
-peri_semi=Frame(root,background="#000000")
-persqrt=Frame(root,background="#000000")
+areas=Frame(root,background="#434343")
+area_tri=Frame(root,background="#434343")
+area_sq=Frame(root,background="#434343")
+area_rec=Frame(root,background="#434343")
+peris=Frame(root,background="#434343")
+peri_peri=Frame(root,background="#434343")
+peri_semi=Frame(root,background="#434343")
+persqrt=Frame(root,background="#434343")
 expr_inp = Entry(f2,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=34)
 expr_inp.grid(row=1,columnspan=6,padx=(0,0),pady=(0,0),ipadx=10,ipady=5,sticky=E+W)
+
+def temp_func(event):
+	if event.keycode==104:
+		action_solve(expr_inp)
+root.bind('<Return>', lambda event: action_solve(expr_inp))
+root.bind('<Key>', temp_func)
+
+
 b_7 = Button(f3, text="7",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command= lambda: action_append(expr_inp,'7'))
 b_7.grid(row=1, column=1,columnspan=1,padx=(10,5),pady=(10,5),sticky=E+W)
 b_8 = Button(f3, text="8",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command= lambda: action_append(expr_inp,'8'))
@@ -481,50 +490,50 @@ def construct_main_calc():
 #area_sq.pack(fill=BOTH,expand=1)
 #area_rec.pack(fill=BOTH,expand=1)
 
-arbk = Button(areas, text="Back",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command=lambda:construct_main_calc())
+arbk = Button(areas, text="Back",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 10),command=lambda:construct_main_calc())
 area_radio_control=IntVar()
-rasq = Radiobutton(areas, text="Square's Area", variable=area_radio_control, value=1,command=lambda: switch_areas_sq())
-ratri = Radiobutton(areas, text="Triangle's Area", variable=area_radio_control, value=2,command=lambda: switch_areas_tri())
-rarec = Radiobutton(areas, text="Rectangle's Area", variable=area_radio_control, value=3,command=lambda: switch_areas_rec())
+rasq = Radiobutton(areas, text="Square's Area", variable=area_radio_control,highlightthickness=0,value=1,bg="#434343",fg="#FFFFFF",selectcolor="#434343",command=lambda: switch_areas_sq(),font=("Helvetica", 12))
+ratri = Radiobutton(areas, text="Triangle's Area", variable=area_radio_control,highlightthickness=0, value=2,bg="#434343",fg="#FFFFFF",selectcolor="#434343",command=lambda: switch_areas_tri(),font=("Helvetica", 12))
+rarec = Radiobutton(areas, text="Rectangle's Area", variable=area_radio_control,highlightthickness=0, value=3,bg="#434343",fg="#FFFFFF",selectcolor="#434343",command=lambda: switch_areas_rec(),font=("Helvetica", 12))
 
-arbk.grid(row=1,column=8,columnspan=2,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
+arbk.grid(row=1,column=8,columnspan=2,padx=(10,5),pady=(5,5),ipadx=2,ipady=2,sticky=E+W)
 rasq.grid(row=2,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 ratri.grid(row=2,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 rarec.grid(row=2,column=7,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 
 
-atl1 = Label(area_tri, text="Side A")
-atl2 = Label(area_tri, text="Side B")
-atl3 = Label(area_tri, text="Side C")
-atl4 = Button(area_tri, text="Area of Triangle",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command=lambda:find_triarea(ate1,ate2,ate3,ate4))
+atl1 = Label(area_tri, text="Side A",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+atl2 = Label(area_tri, text="Side B",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+atl3 = Label(area_tri, text="Side C",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+atl4 = Button(area_tri, text="Area of Triangle",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 11),command=lambda:find_triarea(ate1,ate2,ate3,ate4))
 ate1 = Entry(area_tri,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 ate2 = Entry(area_tri,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 ate3 = Entry(area_tri,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
-ate4 = Entry(area_tri,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
+ate4 = Entry(area_tri,highlightthickness=0,font=("Helvetica", 10),borderwidth=0,justify=RIGHT,width=10)
 atl1.grid(row=1,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 atl2.grid(row=1,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 atl3.grid(row=1,column=7,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 ate1.grid(row=2,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 ate2.grid(row=2,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 ate3.grid(row=2,column=7,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
-atl4.grid(row=3,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
+atl4.grid(row=3,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=0,ipady=0,sticky=E+W)
 ate4.grid(row=3,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 
 
-aql1 = Label(area_sq, text="Side")
-aql2 = Button(area_sq, text="Area of Square",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command=lambda:find_sqarea(aqe1,aqe2))
+aql1 = Label(area_sq, text="Side",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+aql2 = Button(area_sq, text="Area of Square",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 11),command=lambda:find_sqarea(aqe1,aqe2))
 aqe1 = Entry(area_sq,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 aqe2 = Entry(area_sq,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 aql1.grid(row=1,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 aqe1.grid(row=1,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
-aql2.grid(row=2,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
+aql2.grid(row=2,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=0,ipady=0,sticky=E+W)
 aqe2.grid(row=2,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 
 
 
-arl1 = Label(area_rec, text="Length")
-arl2 = Label(area_rec, text="Breadth")
-arl3 = Button(area_rec, text="Area of Rectangle",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command=lambda:find_recarea(are1,are2,are3))
+arl1 = Label(area_rec, text="Length",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+arl2 = Label(area_rec, text="Breadth",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+arl3 = Button(area_rec, text="Area of Rectangle",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 11),command=lambda:find_recarea(are1,are2,are3))
 are1 = Entry(area_rec,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 are2 = Entry(area_rec,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 are3 = Entry(area_rec,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
@@ -532,20 +541,20 @@ arl1.grid(row=1,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,st
 are1.grid(row=1,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 arl2.grid(row=2,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 are2.grid(row=2,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
-arl3.grid(row=3,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
+arl3.grid(row=3,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=0,ipady=0,sticky=E+W)
 are3.grid(row=3,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 
-pebk = Button(peris, text="Back",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command=lambda:construct_main_calc())
+pebk = Button(peris, text="Back",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 10),command=lambda:construct_main_calc())
 peea_radio_control=IntVar()
-pesq = Radiobutton(peris, text="Perimeter", variable=peea_radio_control, value=1,command=lambda: switch_peri_peri())
-petri = Radiobutton(peris, text="SemiPerimter", variable=peea_radio_control, value=2,command=lambda: switch_peri_semi())
-pebk.grid(row=1,column=5,columnspan=2,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
+pesq = Radiobutton(peris, text="Perimeter", variable=peea_radio_control, value=1,highlightthickness=0,bg="#434343",fg="#FFFFFF",selectcolor="#434343",command=lambda: switch_peri_peri(),font=("Helvetica", 12))
+petri = Radiobutton(peris, text="SemiPerimter", variable=peea_radio_control, value=2,highlightthickness=0,bg="#434343",fg="#FFFFFF",selectcolor="#434343",command=lambda: switch_peri_semi(),font=("Helvetica", 12))
+pebk.grid(row=1,column=5,columnspan=2,padx=(10,5),pady=(5,5),ipadx=2,ipady=2,sticky=E+W)
 pesq.grid(row=2,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 petri.grid(row=2,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 
-prl1 = Label(peri_peri, text="No. of sides")
-prl2 = Label(peri_peri, text="Length of side")
-prl3 = Button(peri_peri, text="Perimeter",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command=lambda:find_peri(pre1,pre2,pre3))
+prl1 = Label(peri_peri, text="No. of sides",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+prl2 = Label(peri_peri, text="Length of side",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+prl3 = Button(peri_peri, text="Perimeter",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 11),command=lambda:find_peri(pre1,pre2,pre3))
 pre1 = Entry(peri_peri,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 pre2 = Entry(peri_peri,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 pre3 = Entry(peri_peri,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
@@ -553,12 +562,12 @@ prl1.grid(row=1,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,st
 pre1.grid(row=1,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 prl2.grid(row=2,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 pre2.grid(row=2,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
-prl3.grid(row=3,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
+prl3.grid(row=3,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=1,ipady=1,sticky=E+W)
 pre3.grid(row=3,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 
-spl1 = Label(peri_semi, text="No. of sides")
-spl2 = Label(peri_semi, text="Length of side")
-spl3 = Button(peri_semi, text="Semi-Perimeter",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command=lambda:find_semi(spe1,spe2,spe3))
+spl1 = Label(peri_semi, text="No. of sides",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+spl2 = Label(peri_semi, text="Length of side",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+spl3 = Button(peri_semi, text="Semi-Perimeter",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 11),command=lambda:find_semi(spe1,spe2,spe3))
 spe1 = Entry(peri_semi,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 spe2 = Entry(peri_semi,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 spe3 = Entry(peri_semi,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
@@ -566,19 +575,19 @@ spl1.grid(row=1,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,st
 spe1.grid(row=1,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 spl2.grid(row=2,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 spe2.grid(row=2,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
-spl3.grid(row=3,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
+spl3.grid(row=3,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=0,ipady=0,sticky=E+W)
 spe3.grid(row=3,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 
 
-pqbk = Button(persqrt, text="Back",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command=lambda:construct_main_calc())
-pql1 = Label(persqrt, text="Percentage")
-pql2 = Button(persqrt, text="Percentage Sqrt",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 12),command=lambda:find_persqrt(pqe1,pqe2))
+pqbk = Button(persqrt, text="Back",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 10),command=lambda:construct_main_calc())
+pql1 = Label(persqrt, text="Percentage",highlightthickness=0,bg="#434343",fg="#FFFFFF",font=("Helvetica", 12))
+pql2 = Button(persqrt, text="Percentage Sqrt",bg="#434343",fg="#FFFFFF",highlightthickness=0,font=("Helvetica", 11),command=lambda:find_persqrt(pqe1,pqe2))
 pqe1 = Entry(persqrt,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
 pqe2 = Entry(persqrt,highlightthickness=0,font=("Helvetica", 12),borderwidth=0,justify=RIGHT,width=10)
-pqbk.grid(row=1,column=5,columnspan=2,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
+pqbk.grid(row=1,column=5,columnspan=2,padx=(10,5),pady=(5,5),ipadx=2,ipady=2,sticky=E+W)
 pql1.grid(row=2,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 pqe1.grid(row=2,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
-pql2.grid(row=3,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
+pql2.grid(row=3,column=1,columnspan=3,padx=(10,5),pady=(5,5),ipadx=0,ipady=0,sticky=E+W)
 pqe2.grid(row=3,column=4,columnspan=3,padx=(10,5),pady=(5,5),ipadx=10,ipady=5,sticky=E+W)
 
 
